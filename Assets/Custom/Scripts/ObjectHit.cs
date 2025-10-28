bc7bf0c5ece635cad1841998745e46c3da092240
Siddube <prajwal.be.21@gmail.com>
2025-10-28 14:32:36 +0530

    add object hit class to handle collision of objects with the player

---
+++ Assets/Custom/Scripts/ObjectHit.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// This class handles the logic when an object is hit by the player.
+/// </summary>
+public class ObjectHit : MonoBehaviour
+{
+    // Reference to the object's renderer
+    private Renderer objectRenderer;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        objectRenderer = GetComponent<Renderer>();
+    }
+
+    /// <summary>
+    /// Called when this collider/rigidbody has begun touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            objectRenderer.material.color = Color.black;
+        }
+    }
+}
